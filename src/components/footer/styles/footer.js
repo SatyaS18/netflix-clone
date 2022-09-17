7da7097e46493f0fc882f7c862862cafb1315ad1
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-09-17 22:05:02 +0530

    css added to footer

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -27,3 +27,27 @@ export const Row = styled.div`
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   } ;
 `;
+
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 20px;
+  font-size: 14px;
+  text-decoration: none;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Title = styled.p`
+  font-size: 18px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Break = styled.p`
+  flex-basis: 100%;
+  height: 0;
+`;
