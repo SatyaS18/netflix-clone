a8631630aad6fdba0b97b574e74a53ea2732ef59
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-09-19 22:59:38 +0530

    accordian completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { JumbotronContainer } from "./containers/jumbotron";
+import { FaqsContainer } from "./containers/faqs";
 import { FooterContainer } from "./containers/footer";
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -43,16 +43,24 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   const { toggleShow, setToggleShow } = useContext(ToggleContext);
   return (
-    <Header
-      onclick={() => setToggleShow((toggleShow) => !toggleShow)}
-      {...restProps}
-    >
+    <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
       {children}
+
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="open" />
+      )}
     </Header>
   );
 };
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
   const { toggleShow } = useContext(ToggleContext);
-  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+  //   return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+  return (
+    <Body className={toggleShow ? "open" : "closed"} {...restProps}>
+      <span>{children}</span>
+    </Body>
+  );
 };
