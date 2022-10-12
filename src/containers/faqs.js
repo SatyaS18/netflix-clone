8bcf0199add11662276fb926852a56b8aaa316af
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-10-12 18:08:47 +0530

    added optform in the faqs contatiner

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 import faqsData from "../fixtures/faqs.json";
 
 export function FaqsContainer() {
@@ -14,6 +14,15 @@ export function FaqsContainer() {
           </Accordion.Item>
         ))}
       </Accordion.Frame>
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Text>
+          Ready to watch? Enter your email address to create or restart your
+          membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
